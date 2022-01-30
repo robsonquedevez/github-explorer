4ccf78fe366cbcd0d613262b45422537b1e1a900
Robson Quedevez <robson.quedevez@hotmail.com>
2022-01-30 13:15:44 -0300

    Created components and fetch api github repos

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,10 @@
+export function RepositoryItem(props) {
+    return (
+        <li>
+            <strong>{props.repository?.name ?? 'Default'}</strong>
+            <p>{props.repository?.description ?? 'Default'}</p>
+
+            <a href={props.repository?.html_url ?? "#"}>Acessar respositário</a>
+        </li>
+    );
+}
